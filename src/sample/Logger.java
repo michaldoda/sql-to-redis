0404ed786141b1b459489a68611e88b91b64f6ef
Michal Doda <michal.doda@wp.pl>
2016-11-11 22:01:30 +0100

    init

---
+++ src/sample/Logger.java
@@ -0,0 +1,8 @@
+package sample;
+
+
+public class Logger {
+    public static void info(String logText) {
+
+    }
+}
---
+++ src/sample/Main.java
@@ -0,0 +1,24 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("SQL2Redis - import data from SQL to Redis");
+        primaryStage.setScene(new Scene(root, 920, 620));
+        primaryStage.setResizable(false);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
